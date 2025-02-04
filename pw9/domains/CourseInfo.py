e4c9a12c4a5ee171fc0f26acc0c8ca98b24be630
kien <kiendt.23bi14238@usth.edu.vn>
2025-02-04 14:26:29 +0700

    Copy pw8 to pw9

---
+++ pw9/domains/CourseInfo.py
@@ -0,0 +1,6 @@
+# Class CourseInfo (for encapsulating course data)
+class CourseInfo:
+    def __init__(self, course_id,name,course_credit):
+        self.id = course_id
+        self.name = name
+        self.credit = course_credit
---
+++ pw9/domains/StudentInfo.py
@@ -0,0 +1,6 @@
+# Class StudentInfo (for encapsulating student data)
+class StudentInfo:
+    def __init__(self, student_id, name, dob):
+        self.id = student_id
+        self.name = name
+        self.dob = dob
